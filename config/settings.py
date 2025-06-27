04adae1ca6d6cdd0c86f8e5f7c73b919dc2f0ae8
Shohruz Nuraddinov <shohruz2042@gmail.com>
2025-06-27 15:56:28 +0500

    fix: config settings

--- config/settings.py
+++ config/settings.py
@@ -28,3 +28,5 @@ ALGORITHM = "HS256"
 REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
 REDIS_DB = int(os.environ.get("REDIS_DB", 0))
+
+print(REDIS_HOST, REDIS_PORT, REDIS_DB)
