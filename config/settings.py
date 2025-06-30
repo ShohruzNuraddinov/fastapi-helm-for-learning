a2c40f69a85f8552e38a496ff0152f6e78e69096
Shohruz Nuraddinov <shohruz2042@gmail.com>
2025-06-30 17:01:18 +0500

    fix: helm

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print(os.environ)
 # Database Configuration
 DATABASE_HOST = os.environ.get("DATABASE_HOST", "localhost")
 DATABASE_PORT = int(os.environ.get("DATABASE_PORT", 5432))
